5951bd4d0dae13720b89270ec9eaecd45fe26b07
DilanLivera <DilanLivera@users.noreply.github.com>
2019-07-12 17:53:32 +1000

    setup basic layout to get data from the api and display as a list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hacker News</h1>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      storeis: []
+    };
+  }  
+
+  componentDidMount() {
+    const topStories = 'https://hacker-news.firebaseio.com/v0/topstories.json';
+    const storyUrlBase = 'https://hacker-news.firebaseio.com/v0/item/';
+
+    fetch(topStories)
+      .then(data => data.json())
+      .then(data => data.map(id => {
+        const url = `${storyUrlBase}${id}.json`;
+        return fetch(url).then( data => data.json())
+      }))
+      .then(promises => Promise.all(promises))
+      .then(stories => this.setState({ stories }));
+  }  
+
+  render() {
+    let views = <div>Loading...</div>;
+
+    const { stories } = this.state;
+    if(stories && stories.length > 0) {
+      views = stories.map( story => (
+        <p key={story.id}>
+          <a href={story.url}>{ story.title }</a> from <strong>{ story.by }</strong>
+        </p>
+      ));
+    }
+
+    return (
+      <div className="App">
+        <h2>Hacker News Top Stories</h2>
+        { views }
+      </div>
+    );
+  }  
 }
 
 export default App;
