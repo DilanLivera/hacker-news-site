f0aa0c6931c05f6ac52f9f03ca8d297356c3f646
DilanLivera <DilanLivera@users.noreply.github.com>
2019-07-15 16:38:04 +1000

    update Header and ListContainer's btn styles

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 
 function Header() {
   return (
-    <header className="App-header">
+    <header className="app-header">
       <h1 className='title'>Hacker News</h1>
     </header>
   );
