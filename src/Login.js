b836dee236d71d7c221f3785b01cda5bcf1fda40
Dilan Livera <dylan_yahampath@yahoo.com>
2019-07-30 16:37:22 +1000

    add work sans font to index.css file and fix Login component bug

--- src/Login.js
+++ src/Login.js
@@ -5,9 +5,9 @@ import './Login.css'
 function Login() {
   return (
     <div className='login-form'>
+      <input type="text" placeholder="username"  name="username"/>
+      <input type="password" placeholder = "password" name="password"/>
       <Link to={'./story-list' }>
-        <input type="text" placeholder="username"  name="username"/>
-        <input type="password" placeholder = "password" name="password"/>
         <button>Log in</button>
       </Link>
     </div>
