88d56f0440a07393597ab0e8636957a7fc7436f2
Dilan Livera <dylan_yahampath@yahoo.com>
2019-08-03 16:19:51 +1000

    add skeleton for Comment component and change Article component into a class Component

--- src/Article.js
+++ src/Article.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Article.css';
 
-const Article = (props) => {
-  let { id, url, title, by , descendants, time, score } = props.story;
-  let formatedTime = new Date(time*1000).toDateString();
+class Article extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <div className="article">
-      <p key={ id }>
-        <a href={ url } target="_blank" rel="noopener noreferrer">{ title }</a>       
-      </p>
-      <div className="info-container">
-        <div className="user-date">
-          <em>by</em> <cite ><strong>{ by } </strong>on<strong> { formatedTime }</strong></cite>
-        </div>
-        <div className="score-comment">
-          <p className="score"><span role="img" aria-label="thumbs-up" aria-labelledby="dilan-livera">👍</span> { score }</p>
-          <p className="comments"><span role="img" aria-label="comments" aria-labelledby="dilan-livera">💬</span> { descendants }</p>
-        </div>
-      </div>       
-    </div>
-  );
+  render() {
+    let { id, url, title, by , descendants, time, score } = this.props.story;
+    let formatedTime = new Date(time*1000).toDateString();
+
+    return (
+      <div className="article">
+        <p key={ id }>
+          <a href={ url } target="_blank" rel="noopener noreferrer">{ title }</a>       
+        </p>
+        <div className="info-container">
+          <div className="user-date">
+            <em>by</em> <cite ><strong>{ by } </strong>on<strong> { formatedTime }</strong></cite>
+          </div>
+          <div className="score-comment">
+            <p className="score"><span role="img" aria-label="thumbs-up" aria-labelledby="dilan-livera">👍</span> { score }</p>
+            <p className="comments"><span role="img" aria-label="comments" aria-labelledby="dilan-livera" >💬</span> { descendants }</p>
+          </div>
+        </div>       
+      </div>
+    );
+  }
 }
 
 export default Article;
---
+++ src/components/Comment.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Comment.css';
+
+class Comment extends Component {
+  render() {
+    return (<h3>Comment</h3>);
+  }
+}
+
+export default Comment;
