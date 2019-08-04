f214dde1982aa7695859ae20bab3ee3d48724a5a
Dilan Livera <dylan_yahampath@yahoo.com>
2019-08-04 18:40:27 +1000

    format Comment component and style it. Also fix Article component's showComment function to get story comments

--- src/Article.js
+++ src/Article.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import Comment from './components/Comment';
 import './Article.css';
 
 class Article extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      commentList: []
+    }
+    this.showComments = this.showComments.bind(this);
+  }
+
+  showComments(e) {
+    const storyUrlBase = 'https://hacker-news.firebaseio.com/v0/item/';
+    let { kids }  =  this.props.story;
+    let url;
+
+    Promise.all(kids.map(id => {
+        url = `${storyUrlBase}${id}.json`;
+        return fetch(url).then( data => data.json())
+      }))
+      .then(response => Promise.all( response))
+      .then(result => {
+          let commentList = result.map(comment => {
+            const { by, id, text, time } =  comment;
+            return <Comment key={ id } by={ by } text={ text } time={ time } />;
+          });
+          this.setState({ commentList });
+      });
   }
 
   render() {
@@ -21,8 +45,11 @@ class Article extends Component {
           </div>
           <div className="score-comment">
             <p className="score"><span role="img" aria-label="thumbs-up" aria-labelledby="dilan-livera">👍</span> { score }</p>
-            <p className="comments"><span role="img" aria-label="comments" aria-labelledby="dilan-livera" >💬</span> { descendants }</p>
+            <p className="comments"><span className="comment-icon" role="img" aria-label="comments" aria-labelledby="dilan-livera" onClick={ this.showComments }>💬</span> { descendants }</p>
           </div>
+        </div>
+        <div>
+          { this.state.commentList }
         </div>       
       </div>
     );
--- src/components/Comment.js
+++ src/components/Comment.js
@@ -3,7 +3,15 @@ import './Comment.css';
 
 class Comment extends Component {
   render() {
-    return (<h3>Comment</h3>);
+    let { by, text, time } = this.props;
+
+    return (
+      <div className="Comment">
+        <span className="Comment-tip"></span>
+        <em>by</em> <cite ><strong>{ by } </strong>on<strong> { time }</strong></cite>
+        <p>{ text }</p>
+      </div>
+    );
   }
 }
 
