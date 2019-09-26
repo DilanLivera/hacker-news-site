966b53ffc795f73826ea79d99eb15329b03b0560
Dilan Livera <dylan_yahampath@yahoo.com>
2019-09-26 16:29:48 +1000

    change app root path

--- src/App.js
+++ src/App.js
@@ -6,12 +6,6 @@ import ListContainer from './ListContainer';
 import Login from './Login'
 ;
 class App extends Component{
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     storeis: []
-  //   };
-  // }   
 
   render() {
     let btnList = ["new and top stories", "ask hn stories", "show hn stories", "job hn stories"];
@@ -21,14 +15,12 @@ class App extends Component{
         <BrowserRouter>
           <Header />
           <Switch>
-          <Route exact path='/' component={ Login }/>
+            {/* <Route exact path='/' component={ Login }/> */}
+            <Route exact path='/' render={(props) => <ListContainer {...props} btnList={btnList} />} />
             <Route exact path='/login' component={ Login }/>
-            <Route exact path='/story-list' render={(props) => <ListContainer {...props} btnList={btnList} />} />
+            {/* <Route exact path='/story-list' render={(props) => <ListContainer {...props} btnList={btnList} />} /> */}
           </Switch>
         </BrowserRouter>
-
-        {/* <ListContainer btnList={btnList}/>
-        <Login /> */}
       </div>
     );
   }  
