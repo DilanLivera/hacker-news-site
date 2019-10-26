178771d06bc276d249df1bc061aa38853fb993e2
Dilan Livera <dylan_yahampath@yahoo.com>
2019-10-26 18:22:35 +1100

    add functionality to view an article

--- src/Article.js
+++ src/Article.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import Comment from './components/Comment';
-import './Article.css';
+import React, { Component } from "react";
+import Comment from "./components/Comment";
+import "./Article.css";
 
 class Article extends Component {
   constructor(props) {
@@ -8,68 +8,89 @@ class Article extends Component {
     this.state = {
       commentList: [],
       commentsHidden: true
-    }
+    };
     this.showComments = this.showComments.bind(this);
   }
 
   showComments(e) {
-    const storyUrlBase = 'https://hacker-news.firebaseio.com/v0/item/';
-    let { kids }  =  this.props.story;
+    const storyUrlBase = "https://hacker-news.firebaseio.com/v0/item/";
+    let { kids } = this.props.story;
     let url;
     let { commentsHidden, commentList } = this.state;
 
     //only get comments if the commetList is empty, otherwise change commentsHidden
-    if(kids && commentList.length === 0){
+    if (kids && commentList.length === 0) {
       commentsHidden = false;
 
-      Promise.all(kids.map(id => {
-        url = `${storyUrlBase}${id}.json`;
-        return fetch(url).then( data => data.json())
-      }))
-      .then(response => Promise.all( response))
-      .then(comments => {
+      Promise.all(
+        kids.map(id => {
+          url = `${storyUrlBase}${id}.json`;
+          return fetch(url).then(data => data.json());
+        })
+      )
+        .then(response => Promise.all(response))
+        .then(comments => {
           let commentList = comments.map(comment => {
-            const { by, id, text, time, kids } =  comment;
-            let formatedTime = new Date(time*1000).toDateString();
+            const { by, id, text, time, kids } = comment;
+            let formatedTime = new Date(time * 1000).toDateString();
 
-            return <Comment key={ id } by={ by } text={ text } time={ formatedTime } kids={ kids } />;
+            return <Comment key={id} by={by} text={text} time={formatedTime} kids={kids} />;
           });
           this.setState({ commentList, commentsHidden });
-      });
+        });
     } else {
       //only show or hide comments if there are any comments
-      commentsHidden = (kids) ? !commentsHidden : true;
+      commentsHidden = kids ? !commentsHidden : true;
       this.setState({ commentsHidden });
     }
   }
 
   render() {
-    let { id, url, title, by , descendants, time, score } = this.props.story;
-    let formatedTime = new Date(time*1000).toDateString();
-    let { commentsHidden} = this.state;
+    let { id, url, title, by, descendants, time, score } = this.props.story;
+    let formatedTime = new Date(time * 1000).toDateString();
+    let { commentsHidden } = this.state;
 
     return (
-      <div className="article">
-        <p key={ id }>
-          <a href={ url } target="_blank" rel="noopener noreferrer">{ title }</a>       
+      <div className='article'>
+        <p className='article-title' key={id} dataurl={url} onClick={this.props.showArticle}>
+          {title}
         </p>
-        <div className="info-container">
-          <div className="user-date">
-            <em>by</em> <cite ><strong>{ by } </strong>on<strong> { formatedTime }</strong></cite>
+        <div className='info-container'>
+          <div className='user-date'>
+            <em>by</em>{" "}
+            <cite>
+              <strong>{by} </strong>on<strong> {formatedTime}</strong>
+            </cite>
           </div>
-          <div className="score-comment">
-            <p className="score"><span role="img" aria-label="thumbs-up" aria-labelledby="dilan-livera">👍</span> { score }</p>
-            <p className="comments"><span className="comment-icon" role="img" aria-label="comments" aria-labelledby="dilan-livera" onClick={ this.showComments }>💬</span> { descendants }</p>
+          <div className='score-comment'>
+            <p className='score'>
+              <span role='img' aria-label='thumbs-up' aria-labelledby='dilan-livera'>
+                👍
+              </span>{" "}
+              {score}
+            </p>
+            <p className='comments'>
+              <span
+                className='comment-icon'
+                role='img'
+                aria-label='comments'
+                aria-labelledby='dilan-livera'
+                onClick={this.showComments}
+              >
+                💬
+              </span>{" "}
+              {descendants}
+            </p>
           </div>
         </div>
-        {
-          (!commentsHidden) 
-            ? <div>
-                <hr></hr>
-                { this.state.commentList }
-              </div>
-            : ""
-        }
+        {!commentsHidden ? (
+          <div>
+            <hr></hr>
+            {this.state.commentList}
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     );
   }
--- src/ListContainer.js
+++ src/ListContainer.js
@@ -1,24 +1,28 @@
-import React, { Component } from 'react';
-import './ListContainer.css';
-import Article from './Article';
-import Loading from './Loading';
-import Jokes from './jokes';
-import { Pagination } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
+import "./ListContainer.css";
+import Article from "./Article";
+import Loading from "./Loading";
+import Jokes from "./jokes";
+import { Pagination } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-class ListContainer extends Component{
-  constructor(props){
+class ListContainer extends Component {
+  constructor(props) {
     super(props);
     this.state = {
       stories: [],
       storyIdList: 0,
-      pageActive: 0
+      pageActive: 0,
+      showArticle: false,
+      articleUrl: null
     };
     this.handleClick = this.handleClick.bind(this);
     this.firstHandleClick = this.firstHandleClick.bind(this);
     this.lastHandleClick = this.lastHandleClick.bind(this);
     this.prevHandleClick = this.prevHandleClick.bind(this);
     this.nextHandleClick = this.nextHandleClick.bind(this);
+    this.handleArticleOpenClick = this.handleArticleOpenClick.bind(this);
+    this.handleCloseArticleClick = this.handleCloseArticleClick.bind(this);
   }
 
   componentDidMount() {
@@ -26,50 +30,51 @@ class ListContainer extends Component{
   }
 
   async getStory(id) {
-    const storyUrlBase = 'https://hacker-news.firebaseio.com/v0/item/';
+    const storyUrlBase = "https://hacker-news.firebaseio.com/v0/item/";
     const url = `${storyUrlBase}${id}.json`;
-    return await fetch(url).then( data => data.json())
+    return await fetch(url).then(data => data.json());
   }
 
   async displayStories(btn) {
-    const storiesLinks = [{
-      type: "new and top stories",
-      url: "https://hacker-news.firebaseio.com/v0/topstories.json"
-    },
-    {
-      type: "show hn stories",
-      url: "https://hacker-news.firebaseio.com/v0/showstories.json"
-    },
-    {
-      type: "ask hn stories",
-      url: "https://hacker-news.firebaseio.com/v0/askstories.json"
-    },
-    {
-      type: "job hn stories",
-      url: "https://hacker-news.firebaseio.com/v0/jobstories.json"
-    }];
+    const storiesLinks = [
+      {
+        type: "new and top stories",
+        url: "https://hacker-news.firebaseio.com/v0/topstories.json"
+      },
+      {
+        type: "show hn stories",
+        url: "https://hacker-news.firebaseio.com/v0/showstories.json"
+      },
+      {
+        type: "ask hn stories",
+        url: "https://hacker-news.firebaseio.com/v0/askstories.json"
+      },
+      {
+        type: "job hn stories",
+        url: "https://hacker-news.firebaseio.com/v0/jobstories.json"
+      }
+    ];
 
     let getStories = storiesLinks.find(story => story.type === btn);
 
     // get the story ids for a specific title
-    const storyIdList = await fetch(getStories.url)
-                                .then(data => data.json());
+    const storyIdList = await fetch(getStories.url).then(data => data.json());
     const tenIds = storyIdList.filter((id, i) => i < 10);
 
     // get the first 10 stories for the story ids
     const stories = await Promise.all(tenIds.map(id => this.getStory(id)));
     const pageActive = 0;
-    this.setState({ stories, storyIdList, pageActive })
+    this.setState({ stories, storyIdList, pageActive });
   }
 
-  async loadPageStories(pageActive){
+  async loadPageStories(pageActive) {
     let startIndex = pageActive * 10;
     // get the 10 ids depend on the pagination item
     const tenIds = this.state.storyIdList.filter((id, i) => i > startIndex && i < startIndex + 10);
 
     // get the 10 stories for the ids
     const stories = await Promise.all(tenIds.map(id => this.getStory(id)));
-    this.setState({ ...this.state, stories, pageActive })
+    this.setState({ ...this.state, stories, pageActive });
   }
 
   handleClick(evt) {
@@ -77,70 +82,98 @@ class ListContainer extends Component{
     this.loadPageStories(pageActive);
   }
 
-  firstHandleClick(evt){
+  firstHandleClick(evt) {
     this.loadPageStories(0);
   }
 
-  lastHandleClick(evt){
-    const lastPage = Math.floor(this.state.storyIdList.length/10);
+  lastHandleClick(evt) {
+    const lastPage = Math.floor(this.state.storyIdList.length / 10);
     this.loadPageStories(lastPage);
   }
 
-  prevHandleClick(evt){
+  prevHandleClick(evt) {
     let currentPage = this.state.pageActive;
     --currentPage;
-    if(currentPage < 0 ) currentPage = 0;
+    if (currentPage < 0) currentPage = 0;
     this.loadPageStories(currentPage);
   }
 
-  nextHandleClick(evt){
+  nextHandleClick(evt) {
     let currentPage = this.state.pageActive;
-    const lastPage = Math.floor(this.state.storyIdList.length/10);
+    const lastPage = Math.floor(this.state.storyIdList.length / 10);
     ++currentPage;
-    if(currentPage > lastPage) currentPage = lastPage;
+    if (currentPage > lastPage) currentPage = lastPage;
     this.loadPageStories(currentPage);
   }
 
-  render() {  
+  handleArticleOpenClick(evt) {
+    this.setState({ showArticle: true, articleUrl: evt.target.getAttribute("dataurl") });
+  }
+
+  handleCloseArticleClick() {
+    this.setState({ showArticle: false, articleUrl: null });
+  }
+
+  render() {
     let btnList = this.props.btnList;
-    let num = Math.floor((Math.random())*Jokes.length);
+    let num = Math.floor(Math.random() * Jokes.length);
     let loadingText = Jokes[num];
     let loadingTitle = "here's a joke for you, while you wait...";
-    let views = <Loading loadingTitle={ loadingTitle } loadingText={ loadingText } />;
+    let views = <Loading loadingTitle={loadingTitle} loadingText={loadingText} />;
 
     const { stories, storyIdList, pageActive } = this.state;
-    if(stories && stories.length > 0) {
-      views = stories.map( story => (
-        <Article key ={ story.id } story={ story }/>
+    if (stories && stories.length > 0) {
+      views = stories.map(story => (
+        <Article key={story.id} story={story} showArticle={url => this.handleArticleOpenClick(url)} />
       ));
     }
-    
+
     // set up Pagination.Items
-    const numOfItems = Math.ceil(storyIdList.length/10);
+    const numOfItems = Math.ceil(storyIdList.length / 10);
     const paginationItems = [];
-    
-    for(let i = 0; i < numOfItems; i++) {  
-      paginationItems.push(<Pagination.Item key={i} active={ i === pageActive } onClick={ this.handleClick }>{i + 1}</Pagination.Item>)
+
+    for (let i = 0; i < numOfItems; i++) {
+      paginationItems.push(
+        <Pagination.Item key={i} active={i === pageActive} onClick={this.handleClick}>
+          {i + 1}
+        </Pagination.Item>
+      );
     }
 
     const pageList = paginationItems.filter((page, index) => index >= pageActive && index < pageActive + 7);
 
     return (
-      <div className="list-container">
-        <div className="btn-container">
-          { btnList.map( (btn, i) => <button key={ btn } onClick={ this.displayStories.bind(this, btn) } className="sort-btn">{ btn }</button>) }
+      <div className='list-container'>
+        <div className='btn-container'>
+          {btnList.map((btn, i) => (
+            <button key={btn} onClick={this.displayStories.bind(this, btn)} className='sort-btn'>
+              {btn}
+            </button>
+          ))}
         </div>
-        <div className="article-list">
-          { views }
-          <Pagination>
-            <Pagination.First onClick={ this.firstHandleClick } />
-            <Pagination.Prev onClick={ this.prevHandleClick } />
-            { pageList }
-            <Pagination.Ellipsis disabled />
-            <Pagination.Next onClick={ this.nextHandleClick } />
-            <Pagination.Last onClick={ this.lastHandleClick } />
-          </Pagination>
-        </div>
+        {this.state.showArticle ? (
+          <div>
+            <object
+              id='list-container-show-article'
+              type='text/html'
+              data={this.state.articleUrl}
+              aria-label='article container'
+            ></object>
+            <button onClick={this.handleCloseArticleClick}>Close</button>
+          </div>
+        ) : (
+          <div className='article-list'>
+            {views}
+            <Pagination>
+              <Pagination.First onClick={this.firstHandleClick} />
+              <Pagination.Prev onClick={this.prevHandleClick} />
+              {pageList}
+              <Pagination.Ellipsis disabled />
+              <Pagination.Next onClick={this.nextHandleClick} />
+              <Pagination.Last onClick={this.lastHandleClick} />
+            </Pagination>
+          </div>
+        )}
       </div>
     );
   }
@@ -149,5 +182,5 @@ class ListContainer extends Component{
 export default ListContainer;
 
 // when page loads
-  // get the story count for current btn-label
-  // set story
+// get the story count for current btn-label
+// set story
